e709ae9ed0e52aa6ea78d7e92cf020d668ee7df0
TroshaDashina <iluv16@mail.ru>
2026-09-04 12:18:16 +0700

    Умножение матриц

--- first_program.py
+++ first_program.py
@@ -1,16 +1,30 @@
 n,m=[int(i) for i in input().split()]
 
-matrix=[[0]*m for _ in range(n)]
-
-counter=1
+matrix_A=[]
 
 for i in range(n):
-    for j in range(m):
-        if i<=j:
-            matrix[i][j]=counter
-            counter+=1
+    elem=[int(num) for num in input().split()]
+    matrix_A.append(elem)
+
+input()
+
+m,k=[int(i) for i in input().split()]
+
+matrix_B=[]
+
+for i in range(m):
+    elem=[int(num) for num in input().split()]
+    matrix_B.append(elem)
+
+matrix_result=[[0]*k for _ in range(n)]
 
 for i in range(n):
-    for j in range(m):
-        print(str(matrix[i][j]).ljust(3), end=" ")
-    print()
+    for j in range(k):
+        elem=0
+        for h in range(m):
+            elem+=matrix_A[i][h]*matrix_B[h][j]
+        matrix_result[i][j]=elem
+print()
+
+for i in range(n):
+    print(*matrix_result[i])
