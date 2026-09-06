bdb975204d6c1d40368a9eee2c612ac7303c9cfa
TroshaDashina <iluv16@mail.ru>
2026-09-06 14:18:09 +0700

    3 задание... похоже на этом все

--- t-bank.py
+++ t-bank.py
@@ -1,19 +1,33 @@
-n=int(input())
-massiv=[input() for _ in range(n)]
+n,t=[int(i) for i in input().split()]
 
-counter_s=[]
-counter=[]
+T=[int(num) for num in input().split()]
 
-for i in range(n):
-    if massiv[i] in counter_s:
-        counter[counter_s.index(massiv[i])]+=1
-    else:
-        counter_s.append(massiv[i])
-        counter.append(1)
+counter_line=[0]*(n+1)      # счет строк
+counter_column=[0]*(n+1)    # счет столбцов
+counter_diagonal_R=0        # счет главной диагонали
+counter_diagonal_L=0        # счет побочной диагонали
 
-maximum=max(counter)
+flag=False
 
-for i in range(len(counter)):
-    if counter[i]==maximum:
-        print(counter_s[i])
+for k in range(t):
+    a=T[k]
+
+    line=(a-1)//n
+    column=(a-1)%n
+
+    counter_line[line]+=1
+    counter_column[column]+=1
+
+    if line==column:
+      counter_diagonal_R+=1
+    if line+column==n-1:
+      counter_diagonal_L+=1
+      
+    if counter_line[line]==n or counter_column[column]==n or counter_diagonal_R==n or counter_diagonal_L==n:
+        print(k+1)
+        flag=True
+        break
+           
+if not flag:
+    print(-1)
 
